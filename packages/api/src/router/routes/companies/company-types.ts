dc19b4d96a98d67041e57a872505fac93a5dfad4
A.Miller <adamiller1014@gmail.com>
2024-02-15 15:59:57 -0600

    Adding Address Validation

--- packages/api/src/router/routes/companies/company-types.ts
+++ packages/api/src/router/routes/companies/company-types.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const CreateCompanySchema = z.object({
     name: z.string(),
     active: z.boolean(),
+    address: z.string(),
 });
 
 export type CreateCompanyInput = z.infer<typeof CreateCompanySchema>;
