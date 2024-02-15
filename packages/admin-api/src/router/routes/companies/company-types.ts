90840270e622f0401afb7e489460b2dd7fa218af
A.Miller <adamiller1014@gmail.com>
2024-02-15 09:10:53 -0600

    Combining everything

--- packages/admin-api/src/router/routes/companies/company-types.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const CreateCompanySchema = z.object({
-    name: z.string(),
-    address: z.string(),
-    active: z.boolean(),
-});
-
-export type CreateCompanyInput = z.infer<typeof CreateCompanySchema>;
