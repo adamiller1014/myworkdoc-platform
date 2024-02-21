91f3fb7d08e6db07aec3fe3cb6b49978117c9a46
A.Miller <adamiller1014@gmail.com>
2024-02-21 13:54:46 -0600

    Styling form edit

--- apps/web/src/app/case-forms/[caseFormId]/page.tsx
+++ apps/web/src/app/case-forms/[caseFormId]/page.tsx
@@ -1,32 +1,22 @@
 'use client';
 
 import { api } from "@/utils/react";
-import { Box, Button, Tabs } from "@radix-ui/themes";
-import { Sortable } from "@progress/kendo-react-sortable";
+import { Box, Button, Tabs, TextField } from "@radix-ui/themes";
+import { Sortable, SortableItemUIProps, SortableOnDragOverEvent, SortableOnNavigateEvent } from "@progress/kendo-react-sortable";
+import { useState } from "react";
+import { Bars4Icon } from "@heroicons/react/20/solid";
+import { FormField } from "@myworkdoc/api/src/router/routes/case-forms/caseForm";
 
 export default function CaseForm({ params }: { params: { caseFormId: string } }) {
 
     const { data: caseForm, isLoading } = api.caseForms.get.useQuery(parseInt(params.caseFormId));
-
-
-    const SortableItem = (props: any) => {
-
-        const { isDisabled, isActive, style, attributes, dataItem, forwardRef } =
-            props;
-
-        return <>
-            <div ref={forwardRef} style={style} {...attributes} className="bg-white p-3 m-2 rounded-md shadow-sm">
-                {dataItem.title}
-            </div>
-        </>
-    }
 
     if (isLoading) {
         return <div>Loading...</div>
     }
 
     return <>
-        <div className="h-full w-full bg-gray-200">
+        <div className="h-full w-full bg-gray-100">
 
             <div className="flex justify-between  h-17 p-4 bg-gray-50 border-b-2 border-gray-300 ">
 
@@ -35,58 +25,93 @@ export default function CaseForm({ params }: { params: { caseFormId: string } })
                     <Button>Save</Button>
                 </div>
             </div>
-            <div className="ag-theme-alpine h-[calc(100vh-70px)] " >
-
-                <div className="flex h-full">
-                    <div className="w-3/4 p-10 ">
 
 
-                        <div className="flex justify-between bg-white shadow-sm rounded-md min-h-96">
+            <div className="flex h-full ">
+                <div className="w-3/4 p-10 overflow-auto">
+
+
+                    <div className="flex justify-between bg-white shadow-sm rounded-md min-h-96">
 
 
 
-                            <Tabs.Root defaultValue={caseForm?.form_info?.pages[0]?.title}>
-                                <Tabs.List>
-                                    {caseForm?.form_info?.pages.map((page, idx) => (
-                                        <Tabs.Trigger key={idx} value={page.title}>{page.title}</Tabs.Trigger>
-                                    ))}
+                        <Tabs.Root defaultValue={caseForm?.form_info?.pages[0]?.title} className="w-full">
+                            <Tabs.List>
+                                {caseForm?.form_info?.pages.map((page, idx) => (
+                                    <Tabs.Trigger key={idx} value={page.title}>{page.title}</Tabs.Trigger>
+                                ))}
 
-                                </Tabs.List>
+                            </Tabs.List>
 
-                                <Box px="4" pt="3" pb="2">
-                                    {caseForm?.form_info?.pages.map((page, idx) => (
-                                        <Tabs.Content key={idx} value={page.title}>
-                                            <div>
-
-                                                <Sortable
-                                                    data={page.fields}
-                                                    idField="id"
-                                                    itemUI={SortableItem}
-                                                />
-
-
-                                            </div>
-                                        </Tabs.Content>
-                                    ))}
-                                </Box>
-                            </Tabs.Root>
-
-                        </div>
-
+                            <Box px="4" pt="3" pb="2">
+                                {caseForm?.form_info?.pages.map((page, idx) => (
+                                    <Tabs.Content key={idx} value={page.title} >
+                                        <FormFields fields={page.fields} />
+                                    </Tabs.Content>
+                                ))}
+                            </Box>
+                        </Tabs.Root>
 
                     </div>
-                    <div className="w-1/4 p-3 bg-gray-300">
 
-                    </div>
+
                 </div>
+                <div className="w-1/4 p-3 bg-gray-300">
 
-
-
-
+                </div>
             </div>
         </div>
+
 
     </>
 }
 
+function FormFields({ fields }: { fields: FormField[] }) {
 
+    const SortableItem = (props: SortableItemUIProps) => {
+
+        const { isDisabled, isActive, style, attributes, forwardRef } =
+            props;
+
+        const dataItem = props.dataItem as FormField;
+
+        return <>
+            <div ref={forwardRef} style={style} {...attributes} className="bg-gray-50 p-3 m-2 rounded-md shadow-sm">
+                <div className="flex justify-between">
+                    <div className="flex flex-auto flex-col">
+                        <input type="text" className="w-full bg-gray-50 font-semibold" defaultValue={dataItem.title} />
+
+                        <div className="m-2">
+                            <TextField.Input className="text-gray-400" />
+                        </div>
+
+                    </div>
+                    <div className="flex-none">
+                        <Bars4Icon className="h-5 w-5 text-gray-400" />
+                    </div>
+
+                </div>
+
+
+            </div>
+        </>
+    }
+
+    const [fieldItems, setFieldItems] = useState<FormField[]>(fields);
+    const onDragOver = (event: SortableOnDragOverEvent) => {
+        setFieldItems(event.newState as FormField[]);
+    };
+    const onNavigate = (event: SortableOnNavigateEvent) => {
+        setFieldItems(event.newState as FormField[]);
+    };
+
+    return <>
+        <Sortable
+            data={fieldItems}
+            idField="id"
+            itemUI={SortableItem}
+            onDragOver={onDragOver}
+            onNavigate={onNavigate}
+        />
+    </>
+}
